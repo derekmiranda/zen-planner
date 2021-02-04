e204e5c5c05219814dab1ea41f9e7c4c683185e7
Derek Miranda <derek.miranda.1992@gmail.com>
2021-02-04 14:40:02 -0800

    Adds basic mock server

--- client/src/lib/mock-server.js
+++ client/src/lib/mock-server.js
@@ -1,14 +1,45 @@
-import { Server } from "miragejs";
+import { Server, Model, hasMany, belongsTo } from "miragejs";
 
-new Server({
-  urlPrefix: process.env.REACT_APP_API_URL,
-  routes() {
-    this.get("/users/", () => {
-      return [
-        { name: "Angy", surname: "T." },
-        { name: "Chris", surname: "B." },
-        { name: "Juliana", surname: "Crain" },
-      ];
-    });
-  },
-});
+export default function runMockServer() {
+  new Server({
+    urlPrefix: process.env.REACT_APP_API_URL,
+    namespace: "/api",
+
+    models: {
+      user: Model.extend({
+        tasks: hasMany(),
+      }),
+
+      task: Model.extend({
+        user: belongsTo(),
+      }),
+    },
+
+    seeds(server) {
+      let user = server.create("user", {
+        username: "smithjohn",
+        email: "smithjohn@gmail.com",
+      });
+      server.create("task", { user, description: "Visit bank" });
+    },
+
+    routes() {
+      this.get("/:user_id/tasks/get", (schema, request) => {
+        let userId = request.params.user_id;
+        let user = schema.users.find(userId);
+
+        return user.tasks;
+      });
+
+      this.post("/:user_id/tasks/add", (schema, request) => {
+        const body = JSON.parse(request.requestBody);
+        const user = schema.users.find(request.params.user_id);
+
+        return schema.tasks.create({
+          ...body,
+          user,
+        });
+      });
+    },
+  });
+}
