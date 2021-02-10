a39df2649df17ac270fd683d7afa8c81b36a47b1
Derek Miranda <derek.miranda.1992@gmail.com>
2021-02-09 16:55:48 -0800

    Adds new task action types

--- client/src/store/tasks/types.ts
+++ client/src/store/tasks/types.ts
@@ -1,6 +1,10 @@
 import { Task } from "../../types";
 
 export const ADD_TASK = "ADD_TASK";
+export const REMOVE_TASK = "REMOVE_TASK";
+export const COMPLETE_TASK = "COMPLETE_TASK";
+export const FOCUS_TASK = "FOCUS_TASK";
+export const REORDER_TASKS = "REORDER_TASKS";
 
 export interface AddTaskAction {
   type: typeof ADD_TASK;
