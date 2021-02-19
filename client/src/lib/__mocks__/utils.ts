7cfff4944e65fd6314cb4f987d96aac328794809
Derek Miranda <derek.miranda.1992@gmail.com>
2021-02-18 16:15:29 -0800

    Moves noOp to actual utils

--- client/src/lib/__mocks__/utils.ts
+++ client/src/lib/__mocks__/utils.ts
@@ -1,6 +1,3 @@
 export function createUuid() {
   return "1";
 }
-
-// no op function to avoid redundant multiple no op function creation
-export function noOp() {}
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -3,3 +3,6 @@ import { v4 } from "uuid";
 export function createUuid() {
   return v4();
 }
+
+// no op function to avoid redundant multiple no op function creation
+export function noOp() {}
