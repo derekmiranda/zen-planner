fdda1ee0cb989dbc321f327387a73d9358c098e4
Derek Miranda <derek.miranda.1992@gmail.com>
2021-02-19 15:36:36 -0800

    Uses mapDispatchToProps function

--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -1,23 +1,23 @@
 import { connect } from "react-redux";
+import { Dispatch } from "redux";
 
 import AppPresentation from "../components/AppPresentation";
-import {
-  AddTaskAction,
-  ADD_TASK,
-} from "../store/tasks/types";
+import { AddTaskAction, ADD_TASK } from "../store/tasks/types";
 import { AppState, Task } from "../types";
 
 const mapStateToProps = ({ tasks }: AppState) => ({
   tasks,
 });
 
-const mapDispatchToProps = {
-  onAddTask: (task: Task): AddTaskAction => {
-    return {
-      type: ADD_TASK,
-      task,
-    };
-  },
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    onAddTask: (task: Task): AddTaskAction => {
+      return dispatch({
+        type: ADD_TASK,
+        task,
+      });
+    },
+  };
 };
 
 const ConnectedApp = connect<{}, {}, typeof AppPresentation, AppState>(
--- client/src/containers/TaskContainer.tsx
+++ client/src/containers/TaskContainer.tsx
@@ -1,7 +1,7 @@
-
 import { connect } from "react-redux";
+import { Dispatch } from "redux";
 
-import TaskEl from '../components/tasks/TaskEl'
+import TaskEl from "../components/tasks/TaskEl";
 import {
   CompleteTaskToggleAction,
   COMPLETE_TASK_TOGGLE,
@@ -13,40 +13,39 @@ import {
   UPDATE_TASK_DESC,
 } from "../store/tasks/types";
 
-const mapDispatchToProps = {
-  onUpdateTaskDesc: (
-    uuid: string,
-    newDescription: string
-  ): UpdateTaskDescAction => {
-    return {
-      type: UPDATE_TASK_DESC,
-      uuid,
-      newDescription,
-    };
-  },
-  onToggleComplete: (uuid: string): CompleteTaskToggleAction => {
-    return {
-      type: COMPLETE_TASK_TOGGLE,
-      uuid,
-    };
-  },
-  onToggleFocus: (uuid: string): FocusTaskToggleAction => {
-    return {
-      type: FOCUS_TASK_TOGGLE,
-      uuid,
-    };
-  },
-  onRemoveTask: (uuid: string): RemoveTaskAction => {
-    return {
-      type: REMOVE_TASK,
-      uuid,
-    };
-  },
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    onUpdateTaskDesc: (
+      uuid: string,
+      newDescription: string
+    ): UpdateTaskDescAction => {
+      return dispatch({
+        type: UPDATE_TASK_DESC,
+        uuid,
+        newDescription,
+      });
+    },
+    onToggleComplete: (uuid: string): CompleteTaskToggleAction => {
+      return dispatch({
+        type: COMPLETE_TASK_TOGGLE,
+        uuid,
+      });
+    },
+    onToggleFocus: (uuid: string): FocusTaskToggleAction => {
+      return dispatch({
+        type: FOCUS_TASK_TOGGLE,
+        uuid,
+      });
+    },
+    onRemoveTask: (uuid: string): RemoveTaskAction => {
+      return dispatch({
+        type: REMOVE_TASK,
+        uuid,
+      });
+    },
+  };
 };
 
-const connector = connect(
-  null,
-  mapDispatchToProps
-);
+const connector = connect(null, mapDispatchToProps);
 
 export default connector(TaskEl);
